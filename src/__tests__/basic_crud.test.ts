6e16ae74de121167b5c20defe708db35b07cd3c2
saeid-p <pournejati@gmail.com>
2023-11-14 15:29:49 -0800

    Add basic crus tests.

--- src/__tests__/basic_crud.test.ts
+++ src/__tests__/basic_crud.test.ts
@@ -1,3 +1,4 @@
+import { MongoClient, Db, FindOptions } from "mongodb";
 import * as MongoDb from "../mongodb_client";
 
 const DB_NAME = "test_db_1";
@@ -11,10 +12,10 @@ const DOC_PAYLOAD = {
 };
 
 describe("MongoDb CRUD basic commands.", () => {
-  let client;
+  let client: MongoClient;
 
   const getCollection = () => {
-    const db = client.db(DB_NAME);
+    const db: Db = client.db(DB_NAME);
     const collection = db.collection(COLLECTION_NAME);
     return collection;
   };
@@ -27,9 +28,18 @@ describe("MongoDb CRUD basic commands.", () => {
     await client?.close();
   });
 
+  it("Should create a document.", async () => {
+    const collection = getCollection();
+
+    const response = await collection.insertOne(DOC_PAYLOAD);
+
+    expect(response).toBeTruthy();
+    expect(response.acknowledged).toBe(true);
+  });
+
   it("Should create or update a document.", async () => {
     const collection = getCollection();
-    const query = { id: DOC_ID }; // Specify the criteria to find the document
+    const query = { id: DOC_ID };
     const update = {
       $set: DOC_PAYLOAD,
     };
@@ -41,6 +51,93 @@ describe("MongoDb CRUD basic commands.", () => {
     const response = await collection.updateOne(query, update, config);
 
     expect(response).toBeTruthy();
-    expect(response.acknowledged).toBe(true)
+    expect(response.acknowledged).toBe(true);
+  });
+
+  it("Should return collection document count.", async () => {
+    const collection = getCollection();
+
+    const response = await collection.countDocuments();
+
+    expect(response).toBeTruthy();
+  });
+
+  it("Should return all documents in a collection.", async () => {
+    const collection = getCollection();
+    const query = {};
+
+    const documents = await collection.find(query).toArray();
+
+    expect(documents).toBeTruthy();
+    expect(documents.length).toBeTruthy();
+  });
+
+  it("Should return all documents in a collection with projection.", async () => {
+    const collection = getCollection();
+    const query = {};
+    const options = {
+      projection: {
+        _id: false, // You can only mix inclusion and exclusion for _id field.
+        fullName: true,
+      },
+    };
+
+    const documents = await collection.find(query, options).toArray();
+
+    expect(documents).toBeTruthy();
+    expect(documents.length).toBeTruthy();
+    documents.forEach((document) => {
+      expect(document.fullName).toBeTruthy();
+      expect(document.text).toBeFalsy();
+    });
+  });
+
+  it("Should return all documents paginated and sorted.", async () => {
+    const collection = getCollection();
+    const query = {};
+    const options: FindOptions = {
+      skip: 20,
+      limit: 10,
+      sort: {
+        fullName: 1, // 1 for asc.
+        timestamp: -1, // -1 for desc
+      },
+    };
+
+    const documents = await collection.find(query, options).toArray();
+
+    expect(documents).toBeTruthy();
+    expect(documents.length).toEqual(0);
+  });
+
+  it("Should get a document by id.", async () => {
+    const collection = getCollection();
+    const query = { id: DOC_ID };
+
+    const response = await collection.findOne(query);
+
+    expect(response).toBeTruthy();
+    expect(response.id).toEqual(DOC_ID);
+  });
+
+  it("Should get a document by a property.", async () => {
+    const collection = getCollection();
+    const query = { fullName: DOC_PAYLOAD.fullName };
+
+    const response = await collection.findOne(query);
+
+    expect(response).toBeTruthy();
+    expect(response.id).toEqual(DOC_ID);
+  });
+
+  it("Should remove a document by id.", async () => {
+    const collection = getCollection();
+    const query = { id: DOC_ID };
+
+    const response = await collection.deleteOne(query);
+
+    expect(response).toBeTruthy();
+    expect(response.acknowledged).toBe(true);
+    expect(response.deletedCount).toBeTruthy();
   });
 });
